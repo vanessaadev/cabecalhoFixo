7fbcbe2ef768fa06baf3616a64f367ed6a0d8625
unknown <vanessaaraujo77077@gmail.com>
2023-05-19 16:36:43 -0300

    ajuste de cabeçalho fixo sem ajuda do javascript

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header');
-    var container = document.querySelector('.container');
-    var containerRect = container.getBoundingClientRect();
-  
-    if (window.pageYOffset > containerRect.top) {
-      header.style.position = 'fixed';
-    } else {
-      header.style.position = 'absolute';
-    }
-  });
+  var header = document.querySelector('.header');
+  var container = document.querySelector('.container');
+  var containerRect = container.getBoundingClientRect();
+  var content = document.querySelector('.content');
+  var contentRect = content.getBoundingClientRect();
+
+  if (window.pageYOffset > containerRect.top && window.pageYOffset + container.offsetHeight < contentRect.top + content.offsetHeight) {
+    header.style.position = 'fixed';
+  } else {
+    header.style.position = 'sticky';
+  }
+});
